0ed929b976beafd998e8a903a5fe14f4a7b9fd45
RAGAVI34 <108443392+RAGAVI34@users.noreply.github.com>
2022-07-11 07:11:08 +0530

    Add files via upload

---
+++ ex3.rb
@@ -0,0 +1,25 @@
+#print sentence string
+puts "i will count my CHICKENS:"
+# insert curly brace 
+#calculate the math
+puts "HENS #{25 + 30 / 6}"
+puts "ROOSTERS #{100 - 25 * 3 % 4}"
+#print sentence
+puts "now i will counts tha EGGS:"
+#print result by calculate
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#print sentence
+puts "is it TRUE that 3 + 2 < 5 - 7?"
+#compare 3+2 is less than 5-7
+puts 3 + 2 < 5 -7
+#print sentence with string
+puts "what is 3 + 2? #{3 + 2}"
+puts "what is 5 - 7? #{5 - 7}"
+puts "oh, that's why it's FALSE."
+puts "how about some more."
+#print sentence compare whether 5 is greater than -2
+puts "is it greater? #{5 > -2}"
+#print string compare whether 5 is greater than are equal to -2
+puts "is it greater or equal? #{5 >= -2}"
+#print string compare whether 5 is lesser than or equal to -2
+puts "is is LESS or EQUAL? #{5 <=-2}"
